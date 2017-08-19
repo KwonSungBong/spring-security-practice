21a10e7fe2b674933743dbcbc20c2fd3c91cb9af
ksb <rnjstjdqhd3939@gmail.com>
2017-08-19 21:53:19 +0900

    test

---
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by ksb on 2017. 8. 19..
+ */
+@Controller
+@RequestMapping("/web")
+public class WebController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model) {
+        model.addAttribute("test", "test");
+        return "index";
+    }
+
+}
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by ksb on 2017. 6. 10..
+ */
+console.log("index.js");
